69deb7f94435a2497fbb65c531b7fffc826f47ca
shubm23 <shubm23@gmail.com>
2018-06-14 21:02:38 +0530

    Add existing file

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducers from './src/reducers';
+import {Header} from './src/components/common';
+import LibraryList from './src/components/LibraryList';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Header headerText={'Tech Stack'} />
+          <LibraryList/>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
